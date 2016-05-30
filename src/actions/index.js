a43dde90348c27d7df25afcb23eb19703930fbdd
Marck <mercercountynj@gmail.com>
2016-05-29 20:50:16 -0400

    added post functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 
 // define action type
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 // root url for api
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -23,3 +24,11 @@ export function fetchPosts() {
 }
 
 // data flows through reducers 
+export function createPost(props) {
+     const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+     
+     return {
+          type: CREATE_POST,
+          payload: request
+     };
+}
