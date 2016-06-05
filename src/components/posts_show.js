b80edc56967e1deb28ae4f63473d17627f19ec6c
Marck <mercercountynj@gmail.com>
2016-06-05 19:24:52 -0400

    showing fetched single post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -8,11 +8,25 @@ class PostsShow extends Component {
      }
      
      render() {
+          const { post } = this.props;
+          
+          if (!post) {
+               return <div>Loading...</div>;
+          }
+          
           return (
                <div>
-                    Show post {this.props.params.id}</div>
+                    <h3>{post.title}</h3>
+                    <h6>Categories: {post.catategories}</h6>
+                    <p>{post.content}</p>
+               </div>
           );
      }
 }
+
+function mapStateToProps(state) {
+     return { post: state.posts.post };
+}
+
 // null, not mapping state to props
-export default connect(null, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
